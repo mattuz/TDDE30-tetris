b54e159c225e37ccecf1c2530bc46a4d39a9c846
Matthias <matge373@edu.liu.se>
2020-01-21 19:42:20 +0100

    Övning 2 helt klar (med while)

--- src/se/liu/ida/matge373/tddd78/lab1/Exercise2.java
+++ src/se/liu/ida/matge373/tddd78/lab1/Exercise2.java
@@ -1,12 +1,14 @@
 package se.liu.ida.matge373.tddd78.lab1;
 
+import java.security.spec.RSAOtherPrimeInfo;
+
 public class Exercise2
 {
     public static int sumFor(int min, int max) {
-        int sum = 0;
-	for (int i = 0; min < max; i++) {
-	    min += i;
+	int sum = 0;
+	for (int i = 0; min < max + 1; i++) {
 	    sum += min;
+	    min += 1;
 	}
 	return sum;
     }
@@ -14,7 +16,17 @@ public class Exercise2
     public static void main(String[] args) {
 	int min = 10;
 	int max = 20;
-	System.out.println("Nu har min värdet: " + sumFor(min,max));
+	System.out.println("Summan i for-loopen: " + sumFor(min, max));
+	System.out.println("Summan i while-loopen: " + sumWhile(min, max));
+    }
+
+    public static int sumWhile(int min, int max) {
+	int new_val = 0;
+        while (min < max + 1) {
+            new_val += min;
+	    min += 1;
+	}
+        return new_val;
     }
 }
 
