89a434560c7df9193ece6c56b86211cbda5871fe
Matthias Gerdin <matge373@student.liu.se>
2020-01-23 10:11:29 +0100

    lite mer 1.8 och 1.9

--- src/se/liu/ida/matge373/tddd78/lab1/Exercise8.java
+++ src/se/liu/ida/matge373/tddd78/lab1/Exercise8.java
@@ -5,11 +5,22 @@ import javax.swing.*;
 public class Exercise8
 {
     public static boolean askUser(String question) {
-	JOptionPane.showInputDialog("Answer this question: yes or no?");
-	return JOptionPane.showConfirmDialog(null, "Answer this question: yes or no?", "", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION;
+	JOptionPane.showInputDialog(question);
+	return JOptionPane.showConfirmDialog(null, question, "", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION;
     }
 
-    public static void main(String[] args) {
-	while (askUser())
+    public static void main(String[] args) { ;
+
+	while (!askUser("Quit")) {}
+	while (!askUser("Really?")) {}
+	if ()
+
+//	if (askUser("Quit?") && askUser("Really?")) {
+//	    return;
+//	} else {
+//	}
+
     }
 }
+
+
---
+++ src/se/liu/ida/matge373/tddd78/lab1/Exercise9.java
@@ -0,0 +1,18 @@
+package se.liu.ida.matge373.tddd78.lab1;
+
+import javax.swing.*;
+
+public class Exercise9
+{
+//    public static void main(String[] args) {
+//
+//    }
+//    public static double findRoot(double x) {
+//        double guess = x;
+//	for (int i = 0; i <= 10; i++) {
+//	    guess = guess - (guess*guess-x) / (2*guess);
+//	}
+//	//Double.parseDouble(x) =
+//	//	JOptionPane.showInputDialog("Please input a value");
+//    };
+}
