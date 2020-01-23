8b05942bf23f094b5491121f5ce3743e037278f4
Matthias <matge373@edu.liu.se>
2020-01-23 11:50:29 +0100

    Lab 1 klar

---
+++ src/se/liu/ida/matge373/tddd78/lab1/Excersise10.java
@@ -0,0 +1,14 @@
+package se.liu.ida.matge373.tddd78.lab1;
+
+public class Excersise10
+{
+    public static void main(String[] args) {
+	int tal = 16777217;
+	double decimaltal = tal;
+	int tillbaka = (int)decimaltal;
+	int big = 2147483647;
+	long bigger = (long)big + 1;
+	System.out.println("Tal: "+ tal + ", Decimal: " + decimaltal + ", Tillbaka: " + tillbaka+
+			   ", Big: " + big + ", Bigger: " + bigger);
+    }
+}
