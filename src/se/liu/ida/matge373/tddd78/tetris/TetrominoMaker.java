9acf8335d9bdf51cbae87c439096c4b00b7b5085
Matthias Gerdin <matge373@student.liu.se>
2020-01-30 16:57:50 +0100

    Påbörjat 2.8. Klar med de innan. v2

--- src/se/liu/ida/matge373/tddd78/tetris/TetrominoMaker.java
+++ src/se/liu/ida/matge373/tddd78/tetris/TetrominoMaker.java
@@ -2,6 +2,6 @@ package se.liu.ida.matge373.tddd78.tetris;
 
 public class TetrominoMaker
 {
-    public int getNumberOfTypes() { ... }
-    public Poly getPoly(int n) { ... }
+    //public int getNumberOfTypes() { ... }
+    //public Poly getPoly(int n) { ... }
 }
